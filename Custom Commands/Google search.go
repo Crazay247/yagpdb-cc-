2263da8f78d65e2613ef06be093d3c7452d17eb5
Crazay247 <72492357+Crazay247@users.noreply.github.com>
2021-01-24 12:33:04 +0545

    Add files via upload

---
+++ Custom Commands/Google search.go
@@ -0,0 +1,15 @@
+{{if reFind "google|g" .Cmd}}
+https://www.google.com/search?q={{urlquery (joinStr " " .CmdArgs)}}
+{{else if  reFind "youtube|y" .Cmd }}
+https://www.youtube.com/results?search_query={{urlquery (joinStr " " .CmdArgs)}}
+{{else if reFind "porn|ph" .Cmd }}
+https://www.pornhub.com/video/search?search={{urlquery (joinStr " " .CmdArgs)}}
+{{else if reFind "insta|ig" .Cmd }}
+https://www.instagram.com/{{urlquery (joinStr " " .CmdArgs)}}
+{{else if reFind "facebook|fb" .Cmd }}
+https://www.facebook.com/search/top?q={{urlquery (joinStr " " .CmdArgs)}}
+{{else if reFind "twitter|t" .Cmd }}
+https://twitter.com/search?q={{urlquery (joinStr " " .CmdArgs)}}&src=typed_query&f=user
+{{else if reFind "quora|q" .Cmd }}
+https://www.quora.com/search?q={{urlescape (joinStr " " .CmdArgs)}}
+{{end}}
---
+++ Custom Commands/Welcome 2.go
@@ -0,0 +1,35 @@
+	{{ $username := .User.Username}}
+	{{ $avatar := .User.AvatarURL "256"}}
+	{{ $message := "Welcome+to+server+name"}}
+	{{ $background := "https://cdn.discordapp.com/attachments/755600572215722035/799132031610650684/Welcome_4.png"}}
+	{{ $color := "black"}}
+	{{ $humanage := currentUserAgeHuman}}
+	{{ $advice := (execAdmin "advice") }}
+	{{ $topic := (execAdmin "topic") }}
+	{{ $catfact := (execAdmin "advice") }}
+	{{ $avatar := (joinStr "" "https://cdn.discordapp.com/avatars/" (toString .User.ID) "/" .User.Avatar ".png") }}
+	{{$url := print "https://api.no-api-key.com/api/v2/welcome?username=" 
+	(urlescape .User.Username) 
+	"&user_image=" (.User.AvatarURL "2048")
+	$message 
+		"&background=" $background 
+		"&color=" $color
+		"&text_heading=" (urlquery (print "Welcome to " .Server.Name "!" ))}}
+	{{$embed := cembed
+		"title" "<a:bluefire:785420344072601600>A New Comrade Appears!<a:bluefire:785420344072601600>"
+		"color" (randInt 0 8645612) 
+		"fields" (cslice 
+			(sdict "name" "Advice" "value" $catfact "inline" false) 
+			(sdict "name" "<a:babydance:785378327488757760> Account Age" "value" $humanage "inline" true) 
+			(sdict "name" " <a:tick:785421896194981899> Joined at" "value" .Member.JoinedAt "inline" true) 
+			(sdict "name" " <a:furrydance:785377918682660886> Member Count" "value" (toString .Guild.MemberCount) "inline" true) 
+			
+		) 
+		"author" (sdict "name" " 🔥Bromance Salute!🔥 " "icon_url" "https://media.discordapp.net/attachments/755600572215722035/801474613535178752/logofull.png") 
+		"image" (sdict "url" $url)
+		"timestamp" .Guild.JoinedAt
+	}}
+	{{sendMessage nil .User.Mention}}
+	{{sendMessage nil $embed}}
+	{{addResponseReactions "pepeGunfire:737616060336963726"}}
+	{{addMessageReactions  $embed "happyred:785378521933414420" "babydance:785378327488757760" "settings:785378242760146974" }}
